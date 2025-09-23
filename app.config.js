e63cb90d90547ba41d2ba189cd2f9f1e3fea92b3
Niyaz Khasanov <khasanchik85@gmail.com>
2025-09-23 23:02:34 +0500

    Исправлен формат иконки, удалён app.json, конфиг и структура проекта приведены к стандарту Expo

--- app.config.js
+++ app.config.js
@@ -1,9 +1,28 @@
-// app.config.js
-import 'dotenv/config';
+export default {
+  expo: {
+    name: "Металлург Mobile Clean",
+    slug: "metallurg-mobile-clean",
+    version: "1.0.0",
+    orientation: "portrait",
+    icon: "./assets/icon_mettl.png",
+    userInterfaceStyle: "light",
+    
+    android: {
+      adaptiveIcon: {
+        foregroundImage: "./assets/icon_mettl.png",
+        backgroundColor: "#2c3e50"
+      },
+      package: "com.niyaz0912.metallurgmobileclean"
+    },
+    
+    plugins: ["expo-secure-store"],
+    
+    extra: {
+      eas: {
+        projectId: "f116e8a7-3bc2-437d-a401-eb627c6fd7dd"
+      },
+      apiUrl: "http://159.255.39.41"
+    }
+  }
+};
 
-export default ({ config }) => ({
-  ...config,
-  extra: {
-    apiUrl: process.env.EXPO_PUBLIC_API_URL,
-  },
-});
